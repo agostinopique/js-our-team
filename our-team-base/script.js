76e6929f7b0713b0de146c9a00d931c462f5e7d9
agostinopique <ago.pique@yahoo.it>
2022-04-27 16:39:23 +0200

    First commit;

---
+++ our-team-base/script.js
@@ -0,0 +1,38 @@
+
+/*
+Viene fornito un layout di base in cui è presente **una card di esempio** inserita staticamente nell’html. Questa card serve solo come timbro del markup necessario per ottenere il risultato finale e **dovrà quindi essere rimossa dall’html**.
+
+Aggiungere un file js in cui definire **un array di oggetti** che rappresentano i membri del team. Ogni membro ha le informazioni necessarie cper stampare la relativa card: **Nome, Ruolo e Foto**.
+
+Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell’html, stampare dinamicamente una card per ogni membro del team.
+
+1. Creare una collection all'intrno del quale andare ad inserire tutti i dati di ogni membro del team,
+2. 
+*/
+
+const teamMembers = [
+    {
+        memberImg: './img/wayne-barnett-founder-ceo.jpg',
+        memberName: 'Wayne Barnett',
+        memberRole: 'Founder & CEO',
+    },
+    {
+        memberImg: './img/walter-gordon-office-manager.jpg',
+        memberName: 'Walter Gordon',
+        memberRole: 'Office Manager',
+    },
+    {
+        memberImg: './img/scott-estrada-developer.jpg',
+        memberName: 'Scott Estrada',
+        memberRole: 'Developer',
+    },
+    {
+        memberImg: './img/wayne-barnett-founder-console.jpg',
+        memberName: 'Wayne Barnett',
+        memberRole: 'Founder & CEO',
+    },
+    {
+        memberImg: './img/wayne-barnett-founder-console.jpg',
+        memberName: 'Wayne Barnett',
+        memberRole: 'Founder & CEO',
+    },

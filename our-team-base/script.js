87f74635ce5a00a452ae91906877a3a37073de0f
agostinopique <ago.pique@yahoo.it>
2022-04-28 08:42:55 +0200

    Creata versione alternativa per lággiunta di un nuovo membro;

--- our-team-base/script.js
+++ our-team-base/script.js
@@ -1,16 +1,17 @@
-
 /*
+
 Viene fornito un layout di base in cui è presente **una card di esempio** inserita staticamente nell’html. Questa card serve solo come timbro del markup necessario per ottenere il risultato finale e **dovrà quindi essere rimossa dall’html**.
 
 Aggiungere un file js in cui definire **un array di oggetti** che rappresentano i membri del team. Ogni membro ha le informazioni necessarie cper stampare la relativa card: **Nome, Ruolo e Foto**.
 
 Una volta definita la struttura dati, prendendo come riferimento la card di esempio presente nell’html, stampare dinamicamente una card per ogni membro del team.
 
-1. Creare una collection all'intrno del quale andare ad inserire tutti i dati di ogni membro del team,
-2. 
 */
 
 const teamSection = document.querySelector('div.team-container');
+
+const submitMemberBtn = document.getElementById('addMemberButton');
+
 
 const teamMembers = [
     {
@@ -40,23 +41,121 @@ const teamMembers = [
     },
     {
         memberImg: './img/angela-caroll-chief-editor.jpg',
-        memberName: 'Angela Lopez',
-        memberRole: 'Social Media Manager',
+        memberName: 'Angela Caroll',
+        memberRole: 'Chief Editor',
     }
 ];
 
+// print(teamMembers[0].memberName, teamMembers[0].memberImg, teamMembers[0].memberRole);
 
-for(let key in teamMembers){
-    // console.log(teamMembers[key])
+start();
+
+function print(name, img, role){
+
     teamSection.innerHTML += `
-                            <div class="team-card">
-                                <div class="card-image">
-                                    <img src="${teamMembers[key].memberImg}" alt="${teamMembers[key].memberName}"/>
-                                </div>
-                                <div class="card-text">
-                                    <h3>${teamMembers[key].memberName}</h3>
-                                    <p>${teamMembers[key].memberRole}</p>
-                                </div>
-                            </div>`
+    <div class="team-card">
+        <div class="card-image">
+            <img src="${img}" alt="${name}"/>
+        </div>
+        <div class="card-text">
+            <h3>${name}</h3>
+            <p>${role}</p>
+        </div>
+    </div>`
 }
 
+
+function start(){
+    for(let key in teamMembers){
+        print(teamMembers[key].memberName, teamMembers[key].memberImg, teamMembers[key].memberRole);
+        console.log(teamMembers[key]);
+    }
+}
+
+
+submitMemberBtn.addEventListener('click', newMember);
+
+function newMember(){
+    console.log('Nuovo membro aggiunto!');
+
+    const nameInput = document.getElementById('name').value;
+    const roleInput = document.getElementById('role').value;
+    const imgInput = document.getElementById('image').value;
+
+    let newMember = 
+    {
+        memberImg: imgInput,
+        memberName: nameInput,
+        memberRole: roleInput
+    };
+    console.log(newMember);
+    teamMembers.push(newMember);
+
+    print(newMember.memberName, newMember.memberImg, newMember.memberRole);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+
+function start(){
+
+        print(teamMembers[0].memberImg, teamMembers[0].memberName,teamMembers[0].memberRole);
+
+}
+print();
+
+
+submitMemberBtn.addEventListener('click', memberPush);
+
+
+function memberPush() {
+    console.log('cliccato!');
+
+    const nameInput = document.getElementById('name').value;
+    const roleInput = document.getElementById('role').value;
+    const imgInput = document.getElementById('image').value;
+
+    let newMember = 
+    {
+        memberImg: imgInput,
+        memberName: nameInput,
+        memberRole: roleInput
+    };
+    console.log(newMember);
+    teamMembers.push(newMember);
+
+    print()
+}
+
+function start(){
+
+        teamSection.innerHTML += `
+        <div class="team-card">
+            <div class="card-image">
+                <img src="${teamMembers[key].memberImg}" alt="${teamMembers[key].memberName}"/>
+            </div>
+            <div class="card-text">
+                <h3>${teamMembers[key].memberName}</h3>
+                <p>${teamMembers[key].memberRole}</p>
+            </div>
+
+
+}
+ */
